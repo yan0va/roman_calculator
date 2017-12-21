d9df8241a02b757cbb7da52801e8dd3494194b7c
Anna <anna.yanova@gmail.com>
2017-12-21 11:20:42 +0000

    First passing test

---
+++ lib/roman.rb
@@ -0,0 +1,3 @@
+def input (number)
+  'I'
+end
--- spec/roman_spec.rb
+++ spec/roman_spec.rb
@@ -1,1 +1,8 @@
 require 'roman'
+
+describe 'Roman numeral converter' do
+
+    it 'converts 1 into I' do
+      expect(input(1)).to eq 'I'
+    end
+end
